dc5a9113d32b449081b3d7e520d11998a041e122
Sean Worrell <spworrell@gmail.com>
2023-11-12 20:55:42 -0500

    Db write fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,6 +13,8 @@ const initdb = async () =>
   });
 
 // save to localDB
+// saves when editor is now long main window
+// called from editor.js
 
   export const putDb = async (content) => {
     const jateDb = await openDB("jate", 1);
@@ -20,7 +22,7 @@ const initdb = async () =>
     const store = tx.objectStore("jate");
     const request = store.put({ id:1,  content: content });
     const result = await request;
-      console.log("saved", result);
+      console.log("saved to editor", result);
   };
   
 // get all from localDB
